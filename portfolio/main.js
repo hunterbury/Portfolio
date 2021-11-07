6f57bb652f296e7158ad2eb2bdcaa3e8e8627fce
hunterbury <huntersbury1@gmail.com>
2021-11-07 16:11:46 -0500

    fix nav, add card layout for projects, add icons for footer

--- portfolio/main.js
+++
@@ -1,6 +0,0 @@
-const projects = document.getElementsByClassName('project');
-const languages = document.getElementsByClassName('language');
-//show languages when hovering over a project
-projects.addEventListener("mouseover", (e) => {
-  languages.style.visibility = 'visible';
-});
