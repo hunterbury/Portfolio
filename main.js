194505407f469f1ad3c70fcd7c3b73f98d97c379
hunterbury <huntersbury1@gmail.com>
2021-12-02 13:41:21 -0500

    added styling, burger menu, back to top button

--- main.js
+++ main.js
@@ -2,11 +2,33 @@ const arrow = document.getElementById("down");
 const projects = document.getElementById("projects-title");
 const nav = document.getElementById("nav");
 
-
 arrow.addEventListener('click', () => {
   let rect = projects.getBoundingClientRect();
   let navHeight = nav.offsetHeight;
   window.scrollTo({ behavior: "smooth", top: rect.top - navHeight });
 });
 
+const scrollUp = document.querySelector("#scroll-up");
 
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
