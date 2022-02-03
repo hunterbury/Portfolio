acd775686997b1f9187498be0fa532bd54a241a6
hunterbury <huntersbury1@gmail.com>
2022-02-02 22:11:33 -0500

    styled modal, atyled portfolio section

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ function debounce(func, wait = 20, immediate = true) {
 const sliderImages = document.querySelectorAll('.slide-in', );
 function checkSlide(e) {
   sliderImages.forEach(sliderImage => {
-	const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+	const slideInAt = (window.scrollY + window.innerHeight + 10) - sliderImage.height / 2;
 	const imageBottom = sliderImage.offsetTop + sliderImage.height;
 	const isHalfShown = slideInAt > sliderImage.offsetTop;
 	const isNotScrolledPast = window.scrollY < imageBottom;
